5c09bc005c0f6583c1736265d54ddd0832c8b543
caioslopes <caiolopes.social@gmail.com>
2025-02-06 01:14:28 -0300

    style(Header): add logo image

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { Box } from "../ui/box";
-import { Heading } from "../ui/heading";
-import DateButton from "../DateButton/DateButton";
 import Weekdays from "../Weekdays/Weekdays";
 import { TodoType } from "@/@Types/TodoType";
-import { today } from "@/utils/cDates";
+import { day, monthOrWeekdayName, today } from "@/utils/cDates";
+import { Image } from "react-native";
+import { Text } from "../ui/text";
+import { Heading } from "../ui/heading";
 
 type Props = {
   setFilters: Dispatch<SetStateAction<Partial<TodoType>>>;
@@ -13,9 +14,23 @@ type Props = {
 export default function Header({ setFilters }: Props) {
   return (
     <Box className="p-6">
-      <Box className=" flex flex-row items-center justify-between">
-        <Heading>Lista de Afazeres</Heading>
-        <DateButton date={today()} display="month" />
+      <Box
+        className="flex flex-row items-center justify-between
+      "
+      >
+        <Image
+          source={require("../../assets/images/icon.png")}
+          style={{ width: 80, height: 80 }}
+        />
+
+        <Box>
+          <Text>hoje é</Text>
+          <Heading size="md">
+            {day(today())} de {monthOrWeekdayName(today(), "month", "long")}
+          </Heading>
+        </Box>
+
+        {/* <DateButton date={today()} display="month" /> */}
       </Box>
 
       <Weekdays setFilters={setFilters} />
